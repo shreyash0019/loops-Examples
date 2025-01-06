8e06d3480467973f2ac0f09ca852f970bbaf685b
shreyash0019 <141054759+shreyash0019@users.noreply.github.com>
2025-01-06 13:58:09 +0530

    for loop exapmle

---
+++ for_loopEx8.py
@@ -0,0 +1,19 @@
+"""n = int(input("enter number: "))
+if (n<=0):
+    print("enter vallid value")
+else:
+    prod = 1
+for i in range (1,n+1):
+    prod = prod*i
+else:
+    print(f"prod of {n}= {prod}")"""
+
+line = input("Enter a line of text: ")
+print(f"Given line = {line}")
+
+noa = 0
+for ch in line:
+    if ch.isalpha():
+        noa += 1
+
+print(f"Number of alphabets = {noa}")
