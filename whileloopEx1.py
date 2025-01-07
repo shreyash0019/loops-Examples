9b5d9dfd2d4e4685ff2d1220fdc160a10840faa5
shreyash0019 <141054759+shreyash0019@users.noreply.github.com>
2025-01-07 17:49:11 +0530

    while loop Examples

---
+++ whileloopEx1.py
@@ -0,0 +1,8 @@
+n = int(input("Enter Number you want to generate: "))
+if n <= 0:
+    print("{} is invalid input" . format(n))
+else:
+    i = 1
+    while i <= n:
+        print(i)
+        i += 1
